b0ba448f5edcde61f73895b9131c09b1dc08fdb3
bhatuzdaname <cs12b1010@iith.ac.in>
2015-03-23 10:55:50 +0530

    added CK dataset generation

---
+++ tests/generate_data_kanade.m
@@ -0,0 +1,40 @@
+% Labels
+% 1=anger
+% 2=contempt
+% 3=disgust
+% 4=fear
+% 5=happy
+% 6=sadness
+% 7=surprise
+
+load('cohn-kanade-faces.mat');
+data = zeros(327,64);
+j = [];
+
+for i=1:327
+    disp(i)
+    [points, face1,status] = goodPoints(OG_images{i,1});
+    if (status ==0)
+        j = [j; i];
+        continue;
+    else
+        i2 = Final_images{i,1};
+        face_box = ViolaAndJones(i2,false);
+        if (size(face_box,1) == 0)
+            disp('No face for image2');
+            j = [j; i];
+            continue;
+        else
+            face_box = face_box(1,:);
+            face2 = i2(face_box(2):face_box(2)+face_box(4), face_box(1):face_box(1)+face_box(3),:);
+            [x,y,angle,v] = extractFeatures(face1, face2, points,48,24);
+            data(i,:) = [x;y;angle;v]';
+            if sum(isnan(data(i,:))) ~= 0
+                disp(i);
+            end
+        end
+    end
+end
+idx = setdiff(1:327,j)';
+good_data = data(idx,:);
+good_labels = labels(idx,:);
